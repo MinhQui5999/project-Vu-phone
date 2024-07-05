22704722ead6b899b98e69a9f2cb3431f632af9e
MinhQui5999 <dangminhqui5999@gmail.com>
2024-07-05 20:11:40 +0700

    Add project files.

---
+++ Assest/scripts/script.js
@@ -0,0 +1,66 @@
+$(function () {
+    //carousel
+    $(".owl-slides").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 1,
+            },
+            10000: {
+                item:1,
+            },
+        },
+    });
+   
+    $('.article-carousel').owlCarousel({
+        center: true,
+        dots: false,
+        nav: true,
+        loop: true,
+        margin: 10,
+        responsive: {
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            },
+            10000: {
+                items: 2
+            }
+        }
+    });
+
+    //animmation
+    AOS.init(
+        {
+            once: true,
+        }
+    );
+    //button-croll to top
+    $.backToTop({
+        // background color
+        backgroundColor: '#BE1E2D',
+
+        // text color
+        color: '#FFFFFF',
+    });
+
+    //stick menu
+    $('.snazzymenu').snazzyMenu({
+        theme:"",
+    });
+
+});
